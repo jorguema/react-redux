cac2b046445f78ed6fb49f9a70019202db0fce3e
Jorge Guerola Martínez <jorguema@gmail.com>
2016-09-04 13:24:18 +0200

    app structure, layouts and router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hi");
+import 'babel-polyfill'; //some in ES6 babel can't compile polyfill helps with that
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; //webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
